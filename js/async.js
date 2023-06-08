ed76092263b81daadea7e90771cf88999d64ac5f
Chijioke <wisestcodes@gmail.com>
2023-06-08 12:54:18 -0400

    callback

--- js/async.js
+++ js/async.js
@@ -1,8 +1,32 @@
-"use strict";
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest();
+
+  request.addEventListener("readystatechange", () => {
+    //   console.log(request, request.readyState);
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.responseText);
+    } else if (request.readyState === 4) {
+      callback("Could not fetch data", undefined);
+    }
+  });
+
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+
+  request.send();
+};
+
 console.log(1);
 console.log(2);
-setTimeout(() => {
-  console.log("call back fired");
-}, 2000);
+
+getTodos((err, data) => {
+  console.log("Callback fired");
+
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
+
 console.log(3);
 console.log(4);
