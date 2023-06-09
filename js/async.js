e16c85b5d9bd1888a588ecb389f27a4ad1a03db5
Chijioke <wisestcodes@gmail.com>
2023-06-09 00:22:48 -0400

    fetch

--- js/async.js
+++ js/async.js
@@ -1,26 +1,38 @@
-const getTodos = (resource) => {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
+// const getTodos = (resource) => {
+//   return new Promise((resolve, reject) => {
+//     const request = new XMLHttpRequest();
 
-    request.addEventListener("readystatechange", () => {
-      if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText);
-        resolve(data);
-      } else if (request.readyState === 4) {
-        reject("error getting resource");
-      }
-    });
+//     request.addEventListener("readystatechange", () => {
+//       if (request.readyState === 4 && request.status === 200) {
+//         const data = JSON.parse(request.responseText);
+//         resolve(data);
+//       } else if (request.readyState === 4) {
+//         reject("error getting resource");
+//       }
+//     });
 
-    request.open("GET", resource);
+//     request.open("GET", resource);
 
-    request.send();
-  });
-};
+//     request.send();
+//   });
+// };
 
-getTodos("https://jsonplaceholder.typicode.com/todos/")
+// getTodos("https://jsonplaceholder.typicode.com/todos/")
+//   .then((data) => {
+//     console.log("promise resolved: ", data);
+//   })
+//   .catch((err) => {
+//     console.log("promise rejected: ", err);
+//   });
+
+fetch("https://jsonplaceholder.typicode.com/todos/")
+  .then((response) => {
+    console.log("resolved", response);
+    return response.json();
+  })
   .then((data) => {
-    console.log("promise resolved: ", data);
+    console.log(data);
   })
   .catch((err) => {
-    console.log("promise rejected: ", err);
+    console.log("rejected", err);
   });
